ac194e2053b4aa8f96fdc7d4b4c885f32bca43bf
Kenta-kenpy <101815272+Kenta-kenpy@users.noreply.github.com>
2025-02-13 12:39:14 +0900

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,16 @@
+@page
+@{
+    Layout = null;
+}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Progate</title>
+</head>
+<body>
+    <h1 class="title">C#</h1>
+    <a href="https://github.com/Kenta-kenpy/paiza-python3">C#　コード備忘録</a>
+</body>
+</html>
